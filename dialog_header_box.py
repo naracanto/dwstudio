02e77bd8958961143a9fa8e5c4ad34ffa6c4708d
naracanto <91744875+naracanto@users.noreply.github.com>
2022-02-17 23:39:14 +0100

    Add title, subtitle, logo to dialog header widget

--- dialog_header_box.py
+++ dialog_header_box.py
@@ -18,10 +18,34 @@
 # along with dwStudio.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Qt
+from PySide6.QtSvgWidgets import QSvgWidget
+from PySide6.QtWidgets import QApplication, QGridLayout, QLabel, QWidget
+
+import icons_rc
 
 
 class DialogHeaderBox(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        logo = QSvgWidget()
+        logo.load(":/icons/apps/22/dwstudio.svg")
+
+        title = QLabel("<strong style=\"font-size:large;\">{0}</strong>".format(QApplication.applicationName()))
+        version = QLabel(QApplication.applicationVersion())
+        subtitle = QLabel(self.tr("A front-end tool for the Datawrapper API based on Qt for Python"))
+
+        # Main layout
+        mainLayout = QGridLayout()
+        mainLayout.addWidget(logo, 0, 0, 2, 1)
+        mainLayout.addWidget(title, 0, 1, 1, 1)
+        mainLayout.addWidget(version, 0, 2, 1, 1, Qt.AlignBottom)
+        mainLayout.addWidget(subtitle, 1, 1, 1, 2)
+        mainLayout.setColumnStretch(2, 1)
+        self.setLayout(mainLayout)
+
+        # Logo size
+        height = title.sizeHint().height() + mainLayout.verticalSpacing() + subtitle.sizeHint().height()
+        logo.setFixedSize(height, height)
